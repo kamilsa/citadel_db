1b879f74325c20d01d96de1f204f1e30e2f1c048
grimadas <bulat_nab@mail.ru>
2015-11-21 18:34:10 +0300

    Added driver 0.1b

--- driver/__init__.py
+++ driver/__init__.py
@@ -2,24 +2,26 @@ __author__ = 'bulat'
 import os.path
 from database.hash_db import hash_db
 from relations.student import student
+from Connection import  Connection
 
 class driver:
 
     db_exists = False
 
     def connect(self):
-        self.db_exits = os.path.isfile("/storage/student.txt")
+        self.db = None
+        # TODO : remove this
+        self.db_exits = os.path.exists('storage/student.txt')
+        print(self.db_exits)
         if self.db_exits:
             self.db = hash_db(type=student, from_dump=True)
+            print("Database already exist. Working with dumb")
+
         print("Connected to database ")
+        return Connection(self.db)
 
 
     def execute(self, query):
         pass
 
-    # c = cursor(db=db, filename=db.filename)
-    # c = select_cursor(db=db,filename=db.filename, on_field='name', greater_than="G", less_than="K")
-    c = project_cursor(db=db,filename=db.filename, fields={'name', 'email'})
-    while c.has_next():
-        print c.next()
 
--- driver/cursor.py
+++ driver/cursor.py
@@ -1,6 +1,53 @@
 __author__ = 'bulat'
 
+import sqlparse
+import  database.cursor
+
+def _parse(query, db):
+    sqlparse.parse(query)
+    parsed = sqlparse.parse(query)[0]
+    sql_type =  parsed.get_type()
+    # Primitive checking
+    if (sql_type == "UNKNOWN" or sql_type == None):
+        raise(BaseException("Syntax error in sql query"))
+
+    if sql_type == "SELECT":
+        # Select procedure
+        proj = parsed.tokens[2]
+        print proj
+        if proj.is_whitespace() or str(proj) == '*':
+            # No projection - full select
+
+          #  c = database.cursor.select_cursor(db=db,filename=db.filename, on_field='name', greater_than=None, less_than="B")
+           # c = database.cursor.project_cursor(db=db, filename=db.filename, fields={'name', 'email'}, ordered_on='name')
+            c = database.cursor.cursor(db=db, filename=db.filename)
+            while c.has_next():
+               print c.next()
+
+
+    elif sql_type == "INSERT":
+        pass
+   # elif sql_type == ""
+    # projection :
+
 class Cursor:
 
+    arraysize = 0
+
+    def __init__(self, db):
+        self.db = db
+
     def execute(self, query):
+        _parse(query, self.db)
+
+    def close(self):
+        pass
+
+    def fetchone(self):
+        pass
+
+    def fetchmany(self, size=arraysize):
+        pass
+
+    def fetchall(self):
         pass
